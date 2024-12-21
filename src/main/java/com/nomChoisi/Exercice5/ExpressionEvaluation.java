68d0ac2ab50a78d0e3bcc9557eaef54300d41385
ASMAA ATOUAILAA <asmaa17atouailaa@gmail.com>
2024-12-21 18:24:25 +0100

    le 4eme test qui fonctionne

--- src/main/java/com/nomChoisi/Exercice5/ExpressionEvaluation.java
+++ src/main/java/com/nomChoisi/Exercice5/ExpressionEvaluation.java
@@ -4,19 +4,22 @@ import java.util.Objects;
 
 public class ExpressionEvaluation {
 
-    public static int evaluate (String expression)
-    {
-       if (expression.equals(" ")){
-                return 0;}
-else if(!expression.contains(",")){
-    return Integer.parseInt(expression);
-       }else{
-  String[] tab= expression.split(",");
-  int sum =Integer.parseInt(tab[0])+Integer.parseInt(tab[1]);
-  return sum;
+    public static int evaluate (String expression) {
+        if (expression.equals(" ")) {
+            return 0;
+        } else if (!expression.contains(",")) {
+            return Integer.parseInt(expression);
+        } else if (expression.split(",").length == 2) {
+            String[] tab = expression.split(",");
 
+            int sum = Integer.parseInt(tab[0]) + Integer.parseInt(tab[1]);
+            return sum;
+        } else {
+            String[] tab = expression.split(",");
+            int sume = Integer.parseInt(tab[0]) + Integer.parseInt(tab[1]) + Integer.parseInt(tab[2]);
+
+            return sume;
         }
-    }
 
 
-}
+    }}
